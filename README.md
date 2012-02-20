1b90a6e949c0de37865d3247af4a79ac1ddbc870
Stephen Watkins <stjowa@gmail.com>
2012-02-19 21:59:42 -0500

    Created demos. Created initial JS slide structure.

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 # Slide.js
-## Simple slideshow written in JavaScript.
+## Slideshow written in JavaScript.
 
 _Important: This project has just started. It is very much a work in progress._
 
 Slide.js is a simple, lightweight slideshow written in JavaScript. It's focus is on the behavior of the slideshow, not the presentation. This will result in a powerful API for developers/designers to simply and quickly plug into any existing application. At the same time, leaving them with the flexibility of how they want to present the slideshow.
 
-Slide.js will support:
-
-- Images
-- Videos
-- Audio
+Slide.js will support image and video format.
---
+++ src/Slide.js
@@ -0,0 +1,216 @@
+var Slide = {};
+
+Slide.Show = Class.extend({
+
+	init: function(_target, _options) {
+		this.options = Slide.Util.mergeObjects({
+			'start': 0
+		}, _options);
+
+		this.target = _target;
+		this.items = [];
+		this.current = -1;
+
+		var SLIDE = this;
+		Slide.Util.addEventListener(window, 'resize', function() {
+			SLIDE.items[SLIDE.current].invalidate(SLIDE.target);
+		});
+	},
+
+	addItem: function(_item) {
+		this.items.push(new Slide.Item(_item));
+	},
+
+	addItems: function(_items) {
+		for (var i = 0; i < _items.length; i++) {
+			this.items.push(new Slide.Item(_items[i]));
+		}
+	},
+
+	initialize: function() {
+		this.current = this.options['start'];
+		this.items[this.current].attach(this.target);
+	},
+
+	next: function() {
+		this.items[this.current].detach(this.target);
+		this.current++;
+		if (this.current == this.items.length) {
+			this.current = 0;
+		}
+		this.items[this.current].attach(this.target);
+	},
+
+	previous: function() {
+		this.items[this.current].detach(this.target);
+		this.current--;
+		if (this.current == -1) {
+			this.current = (this.items.length - 1);
+		}
+		this.items[this.current].attach(this.target);
+	}
+
+});
+
+Slide.Item = Class.extend({
+
+	init: function(_src) {
+		var SLIDEITEM = this;
+
+		this.src = _src;
+		this.isLoaded = false;
+
+		this.el = new Image();
+		this.el.onload = function() {
+			SLIDEITEM.isLoaded = true;
+			SLIDEITEM.el.style.removeProperty('display');
+		};
+		this.el.src = _src;
+		this.el.style.display = 'none';
+	},
+
+	attach: function(target) {
+		var SLIDE_ITEM = this;
+		if (!this.isLoaded) {
+			setTimeout(function() {
+				SLIDE_ITEM.attach(target);
+			}, 50);
+		} else {
+			target.appendChild(this.el);
+			this.invalidate(target);
+		}
+	},
+
+	detach: function(target) {
+		target.removeChild(this.el);
+	},
+
+	invalidate: function(target) {
+		var size = Slide.Util.constrain(
+						this.el.offsetWidth,
+						this.el.offsetHeight,
+						target.offsetWidth,
+						target.offsetHeight);
+
+		this.el.style.width = size.width + 'px';
+		this.el.style.height = size.height + 'px';
+	}
+
+});
+
+Slide.Util = {
+
+	mergeObjects: function(obj1, obj2) {
+		if (typeof obj1 == 'undefined') {
+			return {};
+		}
+		if (typeof obj2 == 'undefined') {
+			return obj1;
+		}
+		var obj3 = {};
+		for (var attrname in obj1) {
+			obj3[attrname] = obj1[attrname];
+		}
+		for (attrname in obj2) {
+			obj3[attrname] = obj2[attrname];
+		}
+		return obj3;
+	},
+
+	addEventListener: function(el, ev, fn) {
+		if (el.addEventListener) {
+			el.addEventListener(ev, fn, false);
+		} else if (el.attachEvent) {
+			el.attachEvent('on' + ev, fn);
+		} else {
+			el['on' + ev] = fn;
+		}
+	},
+
+	getStyle: function(el,styleProp) {
+		var x = document.getElementById(el);
+		var y = {};
+		if (x.currentStyle) {
+			y = x.currentStyle[styleProp];
+		} else if (window.getComputedStyle) {
+			y = document.defaultView.getComputedStyle(x, null).getPropertyValue(styleProp);
+		}
+		return y;
+	},
+
+	constrain: function (currentWidth, currentHeight, maxWidth, maxHeight) {
+		if (!maxWidth && !maxHeight) {
+			return {
+				'width': currentWidth,
+				'height': currentHeight
+			};
+		}
+
+		var widthRatio = heightRatio = 1.0;
+		var didWidth = didHeight = false;
+
+		if ( maxWidth > 0 && currentWidth > 0) {// && $current_width > $max_width ) {
+			widthRatio = maxWidth / currentWidth;
+			didWidth = true;
+		}
+
+		if ( maxHeight > 0 && currentHeight > 0) {// && $current_height > $max_height ) {
+			heightRatio = maxHeight / currentHeight;
+			didHeight = true;
+		}
+
+		// Calculate the larger/smaller ratios
+		var smallerRatio = Math.min( widthRatio, heightRatio );
+		var largerRatio  = Math.max( widthRatio, heightRatio );
+
+		var ratio = 1.0;
+		if ((currentWidth * largerRatio) > maxWidth || (currentHeight * largerRatio) > maxHeight) {
+			// The larger ratio is too big. It would result in an overflow.
+			ratio = smallerRatio;
+		} else {
+			// The larger ratio fits, and is likely to be a more "snug" fit.
+			ratio = largerRatio;
+		}
+
+		var w = currentWidth * ratio;
+		var h = currentHeight * ratio;
+
+		// Sometimes, due to rounding, we'll end up with a result like this: 465x700 in a 177x177 box is 117x176... a pixel short
+		// We also have issues with recursive calls resulting in an ever-changing result. Constraining to the result of a constraint should yield the original result.
+		// Thus we look for dimensions that are one pixel shy of the max value and bump them up
+		if (didWidth && (w == maxWidth - 1)) {
+			w = maxWidth; // Round it up
+		}
+		if (didHeight && (h == maxHeight - 1)) {
+			h = maxHeight; // Round it up
+		}
+
+		return {
+			'width': w,
+			'height': h
+		};
+	}
+
+};
+
+function executeOnLoad(node, func) {
+	if (isInDOMTree(node)) {
+		func(node);
+	} else {
+		setTimeout(function() {
+			executeOnLoad(node, func);
+		}, 100);
+	}
+}
+
+function isInDOMTree(node) {
+	return !!(findUltimateAncestor(node).body);
+}
+
+function findUltimateAncestor(node) {
+	var ancestor = node;
+	while (ancestor.parentNode) {
+		ancestor = ancestor.parentNode;
+	}
+	return ancestor;
+}
